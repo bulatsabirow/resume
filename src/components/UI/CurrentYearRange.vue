1fedc7b1d0472fe834cd69d0e34cc0b57aa7c810
bulatsabirow <bulatsabirow@gmail.com>
2024-01-22 19:29:42 +0300

    = optimize code (#18)

---
+++ src/components/UI/CurrentYearRange.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeaderText from '@/components/UI/HeaderText.vue'
+import {STARTING_YEAR} from '@/utils/consts'
+
+const currentYearRange = (startingYear) => {
+  let currYear = new Date().getFullYear()
+  if (currYear === startingYear) {
+    return currYear
+  }
+
+  return `${startingYear}—${currYear}`
+}
+</script>
+
+<template>
+  <HeaderText header="h4">{{ currentYearRange(STARTING_YEAR) }}</HeaderText>
+</template>
+
+<style scoped></style>
--- src/components/UI/ModeSwitcherField.vue
+++ src/components/UI/ModeSwitcherField.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { watch, ref } from 'vue'
+import { watch, ref, computed } from 'vue'
 import { darkLightModeCookieName, getCookie, setCookie } from '@/utils/cookie'
 const isChecked = ref(getCookie(darkLightModeCookieName) === 'light')
 
@@ -8,14 +8,15 @@ watch(isChecked, (newState, oldState) => {
   setCookie(darkLightModeCookieName, newState ? 'light' : 'dark')
   document.body.setAttribute('data-bs-theme', getCookie(darkLightModeCookieName))
 })
+const iconClassName = computed(() => `icon-${isChecked.value ? 'sun' : 'moon'}`)
+const spanClassName = computed(() => isChecked.value ? 'ms-2' : 'me-5')
+const icon = computed(() => ['fas', isChecked.value ? 'sun' : 'moon'])
+console.log(isChecked, iconClassName, spanClassName)
 </script>
 
 <template>
-  <span v-if="isChecked" @click="isChecked = false" class="me-5">
-    <FontAwesomeIcon size="xl" class="icon-moon" :icon="['fas', 'moon']"></FontAwesomeIcon>
-  </span>
-  <span v-else @click="isChecked = true" class="ms-2">
-    <FontAwesomeIcon class="icon-sun" size="xl" :icon="['fas', 'sun']"></FontAwesomeIcon>
+  <span @click="isChecked = !isChecked" :class="spanClassName">
+    <FontAwesomeIcon size="xl" :class="iconClassName" :icon="icon"></FontAwesomeIcon>
   </span>
 </template>
 
