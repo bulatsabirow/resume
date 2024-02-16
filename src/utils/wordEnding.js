2c1cbaedcca4427cb84d08ee2e6c462d5320dd91
bulatsabirow <bulatsabirow@gmail.com>
2024-02-16 14:12:28 +0300

    ! fix bug connected with incorrect word ending definition

--- src/utils/wordEnding.js
+++ src/utils/wordEnding.js
@@ -1,11 +1,28 @@
 export function defineRussianWordEnding(choice) {
-  switch (choice) {
-    case choice % 20 === 1:
-      return 1
-    case choice % 20 <= 4:
-      return 2
-    default:
-      return 3
+  choice %= 100
+
+  if (choice < 5) {
+    return 2
+  } else if (choice < 21) {
+    return 3
+  } else {
+    const remainder = choice % 10
+
+    switch (remainder) {
+      case 0:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        return 3
+
+      case 1:
+        return 1
+
+      default:
+        return 2
+    }
   }
 }
 
