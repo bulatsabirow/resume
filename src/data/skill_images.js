fd03d405a437034191b103fd3aca383efa236fa3
bulatsabirow <bulatsabirow@gmail.com>
2024-04-14 10:53:47 +0300

    + celery skill

--- src/data/skill_images.js
+++ src/data/skill_images.js
@@ -1,9 +1,11 @@
 import postgresImg from '@/assets/images/postgresql.png'
 import djangoImg from '@/assets/images/django.png'
 import redisImg from '@/assets/images/redis.svg'
+import celeryImg from '@/assets/images/celery.png'
 
 export default Object.freeze([
   { title: 'PostgreSQL', src: postgresImg, alt: '<postgresql alt>' },
   { title: 'Django', src: djangoImg, alt: '<django alt>' },
-  { title: 'Redis', src: redisImg, alt: '<redis alt>' }
+  { title: 'Redis', src: redisImg, alt: '<redis alt>' },
+  { title: 'Celery', src: celeryImg, alt: '<celery alt>' }
 ])
